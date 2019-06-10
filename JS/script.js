4404e95b6547bf13aee25d8bd82a3d32c03c0c91
ojasiiitd <ojassaxena03@gmail.com>
2019-06-10 21:23:43 +0530

    update

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,36 @@
 var h = document.getElementById("heading");
 var c = -1;
 
-h.addEventListener("click" , colorbadlo);
+document.body.addEventListener("keydown" , colorbadlo);
 
 function colorbadlo()
 {
-    c++;
+    var kc = event.keyCode;
+    if(kc == 39)
+    {
+        c++;
+    }
+    else if(kc == 37)
+    {
+        c--;
+    }
+    else
+    {
+        kc = null;
+    }
     if(c === 7)
     {
         c = 0;
     }
+    else if(c == -1)
+    {
+        c = 6;
+    }
 
-    var classes = ["is-size-1 has-text-warning" , "is-size-1 has-text-dark" , "is-size-1 has-text-success" , "is-size-1 has-text-info" , "is-size-1 has-text-danger" , "is-size-1 has-text-primary" , "is-size-1 has-text-link"]
-    
-    h.className = classes[c];
+    if(kc)
+    {
+        var classes = ["is-size-1 has-text-warning" , "is-size-1 has-text-dark" , "is-size-1 has-text-success" , "is-size-1 has-text-info" , "is-size-1 has-text-danger" , "is-size-1 has-text-primary" , "is-size-1 has-text-link"] , words = ["are" , "you" , "ready" , "to" , "be" , "fully" , "amazed?"];
+        
+        h.className = classes[c];
+        h.innerHTML = words[c];
 }
